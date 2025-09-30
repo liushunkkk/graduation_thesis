5f70b59cd23bbe2d3fae7de15299c4966356fecd
liushun-ing <1743760069@qq.com>
2025-09-30 21:42:22 +0800

    preview data set

--- python/arbitrary_detection/files/preview.py
+++ python/arbitrary_detection/files/preview.py
@@ -4,5 +4,5 @@ import pandas as pd
 df_pos = pd.read_csv("./positive_data.csv", nrows=100)
 df_neg = pd.read_csv("./negative_data.csv", nrows=100)
 
-df_pos.to_csv("./preview_positive_data.csv", index=False)
-df_neg.to_csv("./preview_negative_data.csv", index=False)
+df_pos.to_csv("./positive_data_preview.csv", index=False)
+df_neg.to_csv("./negative_data_preview.csv", index=False)
