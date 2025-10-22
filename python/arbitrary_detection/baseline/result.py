2e00d8aaa83f7917b283799666215eeb4cde7cdd
liushun-ing <1743760069@qq.com>
2025-10-22 22:31:51 +0800

    补充baseline结果

--- python/arbitrary_detection/baseline/result.py
+++ python/arbitrary_detection/baseline/result.py
@@ -3,8 +3,14 @@ import pandas as pd
 
 df = pd.read_csv("../all_data/datasets/test.csv")
 
-# 只保留其中一列，例如名为 'column_name'
-df = df[['tx_hash']]
+# 只保留['tx_hash', "label"]两列
+df = df[['tx_hash', "label"]]
 
 # 保存到新文件
 df.to_csv("./result.csv", index=False)
+#
+# res = pd.read_csv("./result.csv")
+#
+# res["label"] = df["label"]
+#
+# res.to_csv("./result.csv")
